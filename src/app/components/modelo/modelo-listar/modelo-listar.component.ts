085798cb0d0af671dcceee493526e966e0635f02
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-01 20:12:54 -0300

    nome da Coleção no modelo

--- src/app/components/modelo/modelo-listar/modelo-listar.component.ts
+++ src/app/components/modelo/modelo-listar/modelo-listar.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { Colecao } from 'src/app/Interfaces/colecao';
 import { Modelo } from 'src/app/Interfaces/modelo';
+import { ColecaoService } from 'src/app/services/colecao.service';
 import { ModeloService } from 'src/app/services/modelo.service';
 
 @Component({
@@ -11,19 +14,31 @@ import { ModeloService } from 'src/app/services/modelo.service';
 export class ModeloListarComponent {
 
   modeloList: Modelo[] = [];
+  colecaoList: Colecao[] = [];
+  colecaoCadaModelo: string[] = [];
 
-  constructor(private _service: ModeloService,
-    private route: Router) {}
+  constructor(private _serviceModelo: ModeloService,
+    private _serviceColecao: ColecaoService,
+    private route: Router) { }
 
   async ngOnInit() {
-    await this.listarModelo()
+    await this.listarModelo();
+    await this.listarColecao();
+    this.modeloList.forEach(modelo => {
+      this.colecaoCadaModelo.push(this.colecaoList.filter(colecao => colecao.id == modelo.colecao)[0].nome)
+    })
+
   }
 
-  listarModelo = () => {
-    this._service.listar().subscribe(modelo => this.modeloList = modelo)
+  async listarModelo() {
+    this.modeloList = await firstValueFrom(this._serviceModelo.listar())
   }
 
   editarModelo(id: number) {
     this.route.navigateByUrl(`/home/modelo/cadastrar/${id}`)
+  }
+
+  async listarColecao() {
+    this.colecaoList = await firstValueFrom(this._serviceColecao.listar())
   }
 }
